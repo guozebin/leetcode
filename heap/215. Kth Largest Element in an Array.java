8afb9a3d4e42e127dcd9104afbd43a3aea672336
guozebin <zebin.guo@live.com>
2017-09-07 18:12:15 +0800

    no message

--- heap/215. Kth Largest Element in an Array.java
+++ heap/215. Kth Largest Element in an Array.java
@@ -12,14 +12,11 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(k);
-
+        
         for (int n : nums) {
-            if (pq.size() < k) {
-                pq.add(n);
-            } else if (n > pq.peek()) {
+            pq.add(n);
+            if (pq.size() > k)
                 pq.poll();
-                pq.add(n);
-            }
         }
 
         return pq.peek();
