0db6f9ce772ab40e199711ee63d5bed64dabcff0
guozebin <zebin.guo@live.com>
2017-09-11 12:42:47 +0800

    no message

--- array/561. Array Partition I.java
+++ array/561. Array Partition I.java
@@ -14,9 +14,8 @@ class Solution {
         int sum = 0;
         
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            if (i % 2 == 0)
-                sum += nums[i];
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
         }
         
         return sum;
